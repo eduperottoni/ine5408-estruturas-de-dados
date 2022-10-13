974f3f3f709d2e67045449a8f03b8abfa70ab0b7
caioopra <caioprsilva@gmail.com>
2022-10-13 10:26:28 -0300

    teste.cpp removido e linked_queue funcionando

--- 05_Projeto1/teste.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "headers/linked_queue.h"
-
-int main() {
-    structures::LinkedQueue<int> lista;
-
-    lista.enqueue(1);
-    std::cout << lista.front() << std::endl;
-}
